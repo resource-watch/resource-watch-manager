1e2218e846e90e188f94d020179b9f1a870820a0
David Inga <david.inga@vizzuality.com>
2017-03-15 16:55:44 +0100

    paperclip validation

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -5,10 +5,10 @@ class Partner < ApplicationRecord
   has_attached_file :cover, styles: { large: "1600x600>", medium: "320x180>" }, default_url: "/images/:style/missing.png"
   has_attached_file :icon, styles: { thumb: "25x25>" }, default_url: "/images/:style/missing.png"
 
-  validates_attachment_content_type :logo, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :white_logo, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\z/
-  validates_attachment_content_type :icon, content_type: /\Aimage\/.*\z/
+  validates_attachment_content_type :logo, content_type: /\Aimage/
+  validates_attachment_content_type :white_logo, content_type: /\Aimage/
+  validates_attachment_content_type :cover, content_type: /\Aimage/
+  validates_attachment_content_type :icon, content_type: /\Aimage/
 
   def self.published
     self.where(published: true)
