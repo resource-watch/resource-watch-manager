fc8eab74e301686a09cecebc0cab56a697b780ce
bitTal <albafjez@gmail.com>
2017-03-28 12:16:45 +0200

    Render dataset index table component

--- app/javascript/packs/dataset/index.jsx
+++ app/javascript/packs/dataset/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import DatasetTable from 'rw-components';
 import DatasetTable from 'rw-components/dist/components/Dataset/Table';
 
 const DatasetIndex = () => (
   <div className="row">
     <div className="column small-12">
       <h2>Datasets</h2>
-      <DatasetTable application={['rw']} />
+      <DatasetTable 
+        application={['rw']} 
+        mode='table'
+        columns={[
+          {label: 'name', value: 'name'}, 
+          {label: 'status', value: 'status'},
+          {label: 'provider', value: 'provider'}
+        ]}
+        actions={{
+          show: true,
+          list: [
+            { name: 'Edit', path: 'datasets/:id/edit', show: true },
+            { name: 'Remove', path: 'datasets/:id/remove', show: true }
+          ]
+        }}
+      />
     </div>
   </div>
 );
