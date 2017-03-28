5684dee6e4083e4ee8ee5b4e02654393d57b5952
bitTal <albafjez@gmail.com>
2017-03-28 13:12:16 +0200

    Set values styles

--- app/javascript/packs/dataset/index.jsx
+++ app/javascript/packs/dataset/index.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import DatasetTable from 'rw-components';
 import DatasetTable from 'rw-components/dist/components/Dataset/Table';
 
-const DatasetIndex = () => (
-  <div className="row">
-    <div className="column small-12">
-      <h2>Datasets</h2>
-      <DatasetTable 
-        application={['rw']} 
-        mode='table'
-        columns={[
-          {label: 'name', value: 'name'}, 
-          {label: 'status', value: 'status'},
-          {label: 'provider', value: 'provider'}
-        ]}
-        actions={{
-          show: true,
-          list: [
-            { name: 'Edit', path: 'datasets/:id/edit', show: true },
-            { name: 'Remove', path: 'datasets/:id/remove', show: true }
-          ]
-        }}
-      />
-    </div>
-  </div>
-);
+const classesDic = {
+  status: {
+    succsess: '-succsess',
+    failed: '-failed',
+    saved: '-saved',
+    deleted: '-deleted',
+    pending: '-pending'
+  }
+};
+
+class DatasetIndex extends React.Component {
+
+  getValueClass(type, value) {
+    return classesDic[type][value];
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <div className="column small-12">
+          <h2>Datasets</h2>
+          <DatasetTable 
+            application={['rw']} 
+            mode='table'
+            columns={[
+              { label: 'name', value: 'name' }, 
+              { label: 'status', value: 'status', type: 'status' },
+              { label: 'provider', value: 'provider' }
+            ]}
+            getValueClass={this.getValueClass}
+            actions={{
+              show: true,
+              list: [
+                { name: 'Edit', path: 'datasets/:id/edit', show: true },
+                { name: 'Remove', path: 'datasets/:id/remove', show: true }
+              ]
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 document.addEventListener('DOMContentLoaded', (e) => {
   ReactDOM.render(<DatasetIndex />, document.getElementById('pageContent'));
