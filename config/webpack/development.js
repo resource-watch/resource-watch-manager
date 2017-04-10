7e79ece825cfd6dc8928a61651d8b325cdcc4fd8
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2017-04-10 15:48:41 +0200

    Fix webpack rebuild

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -4,7 +4,7 @@ var merge   = require('webpack-merge')
 var sharedConfig = require('./shared.js')
 
 module.exports = merge(sharedConfig.config, {
-  devtool: 'sourcemap',
+  devtool: 'cheap-module-source-map',
 
   stats: {
     errorDetails: true
