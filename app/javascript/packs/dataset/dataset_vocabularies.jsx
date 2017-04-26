22d7063516ea57738ee15d7a8b4370c8e0f4f199
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2017-04-26 16:34:23 +0200

    fix vocabularies routes confusion

---
+++ app/javascript/packs/dataset/dataset_vocabularies.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { DatasetVocabulariesForm } from 'rw-components';
+
+class DatasetVocabularies extends React.Component {
+
+  render() {
+    return (
+      <div className="row">
+        <div className="column small-12">
+          <DatasetVocabulariesForm
+            application={'rw'}
+            authorization={gon.data.authorization}
+            language="en"
+            dataset={gon.data.dataset_id}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  ReactDOM.render(<DatasetVocabularies />, document.getElementById('pageContent'));
+});
