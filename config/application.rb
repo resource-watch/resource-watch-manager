a019c6d84040fd31cd08efe891c7f1fc4c66739b
Tiago Santos <santos.tiago@gmail.com>
2017-05-16 16:44:25 +0200

    Corrected a bug in reverse proxy. The calls are now preceded by /api

--- config/application.rb
+++ config/application.rb
@@ -44,9 +44,9 @@ module ResourceWatchManager
     unless Rails.env.production?
       config.middleware.insert(0, Rack::ReverseProxy) do
         reverse_proxy_options preserve_host: false
-        reverse_proxy '/dataset', "#{ENV.fetch('APIGATEWAY_URL')}/dataset"
-        reverse_proxy '/widget', "#{ENV.fetch('APIGATEWAY_URL')}/widget"
-        reverse_proxy '/layer', "#{ENV.fetch('APIGATEWAY_URL')}/layer"
+        reverse_proxy '/api/dataset', "#{ENV.fetch('APIGATEWAY_URL')}/dataset"
+        reverse_proxy '/api/widget', "#{ENV.fetch('APIGATEWAY_URL')}/widget"
+        reverse_proxy '/api/layer', "#{ENV.fetch('APIGATEWAY_URL')}/layer"
 
       end
     end
