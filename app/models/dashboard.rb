e91e626028bff38fdd58c67bf6f99897b0c44558
David Inga <david.inga@vizzuality.com>
2017-08-03 13:44:37 +0200

    added dashboard photo

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -3,6 +3,14 @@ class Dashboard < ApplicationRecord
   friendly_id :name, use: %i(slugged)
   validates_presence_of :name
 
+  before_validation :parse_image
+  attr_accessor :image_base
+
+  validates_presence_of :title
+  has_attached_file :photo, styles: { cover: '1280x800>', thumb: '110x110>' }
+  validates_attachment_content_type :photo, content_type: %r{^image\/.*}
+  do_not_validate_attachment_file_type :photo
+
   scope :by_published, -> published { where(published: published) }
 
   def self.fetch_all(options={})
--- app/serializers/dashboard_serializer.rb
+++ app/serializers/dashboard_serializer.rb
@@ -1,3 +1,10 @@
 class DashboardSerializer < ActiveModel::Serializer
-  attributes :id, :name, :slug, :description, :body, :published
+  attributes :id, :name, :slug, :summary, :description, :content, :published, :photo
+
+  def photo
+    {
+      cover: object.photo.url(:cover),
+      thumb: object.photo.url(:thumb)
+    }
+  end
 end
