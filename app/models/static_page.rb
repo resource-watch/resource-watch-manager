72fe32ef0cf537524619682064a50a018419bb00
David Inga <david.inga@vizzuality.com>
2017-08-03 13:50:04 +0200

    fixing dashaboard creation

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -25,13 +25,10 @@ class StaticPage < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: %i(slugged)
 
-  before_validation :parse_image
-  attr_accessor :image_base
-
-  validates_presence_of :title
-  has_attached_file :photo, styles: { cover: '1280x800>' }
-  validates_attachment_content_type :photo, content_type: %r{^image\/.*}
-  do_not_validate_attachment_file_type :photo
+  # validates_presence_of :title
+  # has_attached_file :photo, styles: { cover: '1280x800>' }
+  # validates_attachment_content_type :photo, content_type: %r{^image\/.*}
+  # do_not_validate_attachment_file_type :photo
 
   def should_generate_new_friendly_id?
     new_record?
@@ -60,12 +57,12 @@ class StaticPage < ApplicationRecord
     "#{field} #{direction}"
   end
 
-  private
+  # private
 
-  def parse_image
-    return if image_base.nil?
-    image = Paperclip.io_adapters.for(image_base)
-    image.original_filename = 'file.jpg'
-    self.photo = image
-  end
+  # def parse_image
+  #   return if image_base.nil?
+  #   image = Paperclip.io_adapters.for(image_base)
+  #   image.original_filename = 'file.jpg'
+  #   self.photo = image
+  # end
 end
