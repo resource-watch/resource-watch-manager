f6a30d1cb6aa75e23695143a8a3bddf6b6123a94
rrequero <rrequero@gmail.com>
2017-08-03 18:29:16 +0200

    update ping

--- app/controllers/healths_controller.rb
+++ app/controllers/healths_controller.rb
@@ -1,5 +1,5 @@
 class HealthsController < ApplicationController
   def index
-    render nothing: true, status: 200, content_type: 'text/html'
+    render json: { success: true, message: 'PONG' }, status: 200
   end
 end
