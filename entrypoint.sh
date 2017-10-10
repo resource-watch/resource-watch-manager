873efbd388ed5f0de967a5e0bac3b0a5540c2238
David Inga <david.inga@vizzuality.com>
2017-10-10 11:53:48 +0200

    adding migrations to endpoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@ set -e
 
 case "$1" in
     start)
+        echo "Running Migrations"
+        exec bundle exec rake db:migrate
+
         echo "Running Start"
         exec bundle exec puma -C config/puma.rb
         ;;
