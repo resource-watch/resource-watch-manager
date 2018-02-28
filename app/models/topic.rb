c7b84df57b623bd3755664bcce9c0303e51b2753
Tiago Santos <santos.tiago@gmail.com>
2018-02-28 13:21:41 +0000

    Bug fix on adding images to dashboards and topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -106,7 +106,8 @@ class Topic < ApplicationRecord
 
       if params['temp_id'].present?
         temp = TemporaryContentImage.find(params['temp_id'].first)
-        content_image = ContentImage.create(topic_id: id, image: temp.image)
+        content_image = ContentImage.create(imageable_id: id,
+                                            imageable_type: 'Topic', image: temp.image)
 
         temp.destroy
         content_image
