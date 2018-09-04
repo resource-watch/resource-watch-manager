8725cb28a5f0ed8d3592b8e5b26461ee738543f2
Tiago Garcia <tiagojsag@gmail.com>
2018-09-04 14:05:26 +0200

    Update init script to register with CT

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ set -e
 case "$1" in
     start)
         echo "Running Start"
-        rake db:migrate
-        whenever --update-crontab
-        exec bundle exec puma -C config/puma.rb & (sleep 30; bundle exec rake ct_register_microservice:register)
+#        rake db:migrate
+#        whenever --update-crontab
+        sleep 5 && bundle exec rake ct_register_microservice:register &
+        exec bundle exec puma -C config/puma.rb
         ;;
     *)
         exec "$@"
