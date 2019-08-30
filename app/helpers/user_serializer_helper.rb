105a3701a68f85825831c0f0e1e8e56eb4a4d282
Tiago Santos <santos.tiago@gmail.com>
2019-08-30 11:28:58 +0100

    more logs

--- app/helpers/user_serializer_helper.rb
+++ app/helpers/user_serializer_helper.rb
@@ -5,6 +5,9 @@ module UserSerializerHelper
 
   def self.element(element, users)
     hash = element.as_json
+    Rails.logger.info("<Element> hash: #{hash.inspect}")
+    Rails.logger.info("<Element> string: #{hash['user_id']}")
+    Rails.logger.info("<Element> symbol: #{hash[:user_id]}")
     user_id = hash.delete('user_id')
     Rails.logger.info("<Element> users_id: #{user_id}")
     user = users.select{ |u| u['_id'] == user_id }.first rescue nil
