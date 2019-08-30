46d5dbaafa08f9787b107887871a364eecea5f21
Tiago Santos <santos.tiago@gmail.com>
2019-08-30 11:07:17 +0100

    Added logger to the user serializer

--- app/helpers/user_serializer_helper.rb
+++ app/helpers/user_serializer_helper.rb
@@ -6,7 +6,9 @@ module UserSerializerHelper
   def self.element(element, users)
     hash = element.as_json
     user_id = hash.delete('user_id')
+    Rails.logger.info("<Element> users_id: #{user_id}")
     user = users.select{ |u| u['_id'] == user_id }.first rescue nil
+    Rails.logger.info("<Element> user: #{user.inspect}")
     hash[:user] = user_serializer(user)
     hash
   end
