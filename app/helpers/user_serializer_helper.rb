e5f57ce9d4384771f2c2f0d6bc0e6e8f2b78899d
Tiago Santos <santos.tiago@gmail.com>
2019-08-30 10:42:18 +0100

    Bug fix in users serialization

--- app/helpers/user_serializer_helper.rb
+++ app/helpers/user_serializer_helper.rb
@@ -6,7 +6,7 @@ module UserSerializerHelper
   def self.element(element, users)
     hash = element.as_json
     user_id = hash.delete('user_id')
-    user = users.select { |u| u['_id'] == user_id }
+    user = users.select{ |u| u['_id'] == user_id }.first rescue nil
     hash[:user] = user_serializer(user)
     hash
   end
@@ -16,17 +16,12 @@ module UserSerializerHelper
   def self.user_serializer(user)
     return {} if user.blank?
 
-    begin
-      {
-          id: user['_id'],
-          name: user['name'],
-          role: user['role'],
-          email: user['email'],
-          apps: user.dig('extraUserData', 'apps')
-      }
-    rescue
-      puts "Error on user: #{user.inspect}"
-      return {}
-    end
+    {
+        id: user['_id'],
+        name: user['name'],
+        role: user['role'],
+        email: user['email'],
+        apps: user.dig('extraUserData', 'apps')
+    }
   end
 end
