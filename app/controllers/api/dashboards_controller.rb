5c79e3d4b937b47a498a7956ce0f95d6af8c0ef9
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-11-29 11:15:01 +0000

    Fix typo in error message

--- app/controllers/api/dashboards_controller.rb
+++ app/controllers/api/dashboards_controller.rb
@@ -132,7 +132,7 @@ class Api::DashboardsController < ApiController
   def ensure_is_admin_for_highlighting
     return true if !request.params.dig('data', 'attributes', 'is_highlighted').present?
     return true if request.params.dig('data', 'attributes', 'is_highlighted').present? and @user[:role].eql? "ADMIN"
-    render json: {errors: [{status: '403', title: 'You need to an ADMIN to create/update the is_highlighted attribute of the dashboard'}]}, status: 403
+    render json: {errors: [{status: '403', title: 'You need to be an ADMIN to create/update the is_highlighted attribute of the dashboard'}]}, status: 403
   end
 
   def get_user
