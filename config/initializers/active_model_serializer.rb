4e9cf234942b5ed0dd2f4faa0b37f9cce90df440
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-04 16:38:46 +0000

    Ensure pagination links are correctly built for CT access

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -14,15 +14,19 @@ class CustomPaginationLinks < ActiveModelSerializers::Adapter::JsonApi
   private
 
   def normalize_pagination_links(links)
-    links[:self] = replace_logged_user_query_param(links[:self])
-    links[:first] = replace_logged_user_query_param(links[:first])
-    links[:last] = replace_logged_user_query_param(links[:last])
-    links[:prev] = replace_logged_user_query_param(links[:prev])
-    links[:next] = replace_logged_user_query_param(links[:next])
+    links[:self] = replace_dashboard_correct_url(replace_logged_user_query_param(links[:self]))
+    links[:first] = replace_dashboard_correct_url(replace_logged_user_query_param(links[:first]))
+    links[:last] = replace_dashboard_correct_url(replace_logged_user_query_param(links[:last]))
+    links[:prev] = replace_dashboard_correct_url(replace_logged_user_query_param(links[:prev]))
+    links[:next] = replace_dashboard_correct_url(replace_logged_user_query_param(links[:next]))
   end
 
   def replace_logged_user_query_param(str)
     return !str.nil? ? str.gsub(/loggedUser=[^&]*&/m, '') : str
+  end
+
+  def replace_dashboard_correct_url(str)
+    return !str.nil? ? str.gsub(/api\/dashboards/m, 'v1/dashboard') : str
   end
 end
 
