2da87731ca9a822967a64e301ebcd945c4f7b9a6
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-04 16:23:48 +0000

    Add custom repalcement of query params in pagination links

--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,3 +1,29 @@
 # frozen_string_literal: true
 
-ActiveModelSerializers.config.adapter = :json_api
+class CustomPaginationLinks < ActiveModelSerializers::Adapter::JsonApi
+  def success_document
+    res = super
+
+    if res[:links]
+      normalize_pagination_links(res[:links])
+    end
+
+    return res
+  end
+
+  private
+
+  def normalize_pagination_links(links)
+    links[:self] = replace_logged_user_query_param(links[:self])
+    links[:first] = replace_logged_user_query_param(links[:first])
+    links[:last] = replace_logged_user_query_param(links[:last])
+    links[:prev] = replace_logged_user_query_param(links[:prev])
+    links[:next] = replace_logged_user_query_param(links[:next])
+  end
+
+  def replace_logged_user_query_param(str)
+    return !str.nil? ? str.gsub(/loggedUser=[^&]*&/m, '') : str
+  end
+end
+
+ActiveModelSerializers.config.adapter = CustomPaginationLinks
