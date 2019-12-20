8ac8c9754aaabbba6e79388bb15dc5bf4b8709d5
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-20 11:44:53 +0000

    Add permission check when deleting a dashboard

--- spec/controllers/api/dashboards_delete_spec.rb
+++ spec/controllers/api/dashboards_delete_spec.rb
@@ -8,6 +8,7 @@ describe Api::DashboardsController, type: :controller do
   describe 'DELETE #dashboard' do
     before(:each) do
       @dashboard_private_manager = FactoryBot.create :dashboard_private_manager
+      @dashboard_private_user_1 = FactoryBot.create :dashboard_private_user_1
     end
 
     it 'with no user details should produce a 401 error' do
@@ -17,6 +18,35 @@ describe Api::DashboardsController, type: :controller do
 
       expect(response.status).to eq(401)
       expect(response.body).to include "Unauthorized"
+    end
+
+    it 'with role USER should produce an 403 error' do
+      delete :destroy, params: {
+        id: @dashboard_private_manager[:id],
+        loggedUser: USERS[:USER]
+      }
+
+      expect(response.status).to eq(403)
+      expect(response.body).to include "You need to be either ADMIN or MANAGER and own the dashboard to update/delete it"
+    end
+
+    it 'with role MANAGER but not the owner of the dashboard should produce an 403 error' do
+      delete :destroy, params: {
+        id: @dashboard_private_user_1[:id],
+        loggedUser: USERS[:MANAGER]
+      }
+
+      expect(response.status).to eq(403)
+      expect(response.body).to include "You need to be either ADMIN or MANAGER and own the dashboard to update/delete it"
+    end
+
+    it 'with role MANAGER and the owner of the dashboard should return 204 No Content' do
+      delete :destroy, params: {
+        id: @dashboard_private_manager[:id],
+        loggedUser: USERS[:MANAGER]
+      }
+
+      expect(response.status).to eq(204)
     end
 
     it 'with ADMIN token should delete the dashboard and return 204 No Content' do
