133a3186d1989d15c15c297fe8d2227fba946450
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2019-12-20 12:24:11 +0000

    Add application matching rules between user and dashboard before deleting

--- spec/controllers/api/dashboards_delete_spec.rb
+++ spec/controllers/api/dashboards_delete_spec.rb
@@ -8,7 +8,6 @@ describe Api::DashboardsController, type: :controller do
   describe 'DELETE #dashboard' do
     before(:each) do
       @dashboard_private_manager = FactoryBot.create :dashboard_private_manager
-      @dashboard_private_user_1 = FactoryBot.create :dashboard_private_user_1
     end
 
     it 'with no user details should produce a 401 error' do
@@ -30,29 +29,66 @@ describe Api::DashboardsController, type: :controller do
       expect(response.body).to include "You need to be either ADMIN or MANAGER and own the dashboard to update/delete it"
     end
 
-    it 'with role MANAGER but not the owner of the dashboard should produce an 403 error' do
+    it 'with role MANAGER, NOT the owner of the dashboard, should produce an 403 error' do
+      spoofed_user = USERS[:MANAGER].deep_dup
+      spoofed_user[:id] = "123"
+
       delete :destroy, params: {
-        id: @dashboard_private_user_1[:id],
-        loggedUser: USERS[:MANAGER]
+        id: @dashboard_private_manager[:id],
+        loggedUser: spoofed_user
       }
 
       expect(response.status).to eq(403)
       expect(response.body).to include "You need to be either ADMIN or MANAGER and own the dashboard to update/delete it"
     end
 
-    it 'with role MANAGER and the owner of the dashboard should return 204 No Content' do
+    it 'with role MANAGER, owner of the dashboard, but non-matching applications between user and dashboard should produce an 403 error' do
+      spoofed_user = USERS[:MANAGER].deep_dup
+      spoofed_user[:extraUserData][:apps] = ["fake-app"]
+
       delete :destroy, params: {
         id: @dashboard_private_manager[:id],
-        loggedUser: USERS[:MANAGER]
+        loggedUser: spoofed_user
+      }
+
+      expect(response.status).to eq(403)
+      expect(response.body).to include "Your user account does not have permissions to delete this dashboard"
+    end
+
+    it 'with role MANAGER, owner of the dashboard, and at least one matching application between user and dashboard should return 204 No Content' do
+      spoofed_user = USERS[:MANAGER].deep_dup
+      spoofed_user[:extraUserData][:apps] = ["rw", "gfw"]
+
+      delete :destroy, params: {
+        id: @dashboard_private_manager[:id],
+        loggedUser: spoofed_user
       }
 
       expect(response.status).to eq(204)
     end
 
-    it 'with ADMIN token should delete the dashboard and return 204 No Content' do
+    it 'with role ADMIN, NOT owner of the dashboard, but non-matching applications between user and dashboard should produce an 403 error' do
+      spoofed_user = USERS[:ADMIN].deep_dup
+      spoofed_user[:id] = "123"
+      spoofed_user[:extraUserData][:apps] = ["fake-app"]
+
       delete :destroy, params: {
         id: @dashboard_private_manager[:id],
-        loggedUser: USERS[:ADMIN]
+        loggedUser: spoofed_user
+      }
+
+      expect(response.status).to eq(403)
+      expect(response.body).to include "Your user account does not have permissions to delete this dashboard"
+    end
+
+    it 'with role ADMIN, NOT owner of the dashboard, but at least one matching application between user and dashboard should return 204 No Content' do
+      spoofed_user = USERS[:ADMIN].deep_dup
+      spoofed_user[:id] = "123"
+      spoofed_user[:extraUserData][:apps] = ["rw", "gfw"]
+
+      delete :destroy, params: {
+        id: @dashboard_private_manager[:id],
+        loggedUser: spoofed_user
       }
 
       expect(response.status).to eq(204)
