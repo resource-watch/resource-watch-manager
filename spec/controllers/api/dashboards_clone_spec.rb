c16c7baffd4b32155a246e4984dde2585c3e6cc6
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-01-02 13:04:02 +0000

    Add whitelist of fields for overriding dashboard data when cloning

--- spec/controllers/api/dashboards_clone_spec.rb
+++ spec/controllers/api/dashboards_clone_spec.rb
@@ -10,26 +10,95 @@ describe Api::DashboardsController, type: :controller do
       @dashboard = FactoryBot.create :dashboard_with_widgets
     end
 
-    it 'should perform the cloning' do
+    it 'returns 200 OK with the created dashboard data when providing no data in request body (happy case)' do
       VCR.use_cassette('dataset_widget') do
-        post 'clone', params: {id: '1', loggedUser: USERS[:ADMIN]}
+        post 'clone', params: {id: @dashboard.id, loggedUser: USERS[:ADMIN]}
         expect(response.status).to eq(200)
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['data']['id']).to satisfy { |new_id| new_id != @dashboard['id'] }
+        expect(json_response['data']['attributes']['slug']).to satisfy { |new_slug| new_slug != @dashboard['slug'] }
+
+        expect(json_response['data']['attributes']['name']).to eq(@dashboard['name'])
+        expect(json_response['data']['attributes']['summary']).to eq(@dashboard['summary'])
+        expect(json_response['data']['attributes']['description']).to eq(@dashboard['description'])
+        expect(json_response['data']['attributes']['published']).to eq(@dashboard['published'])
+        expect(json_response['data']['attributes']['user-id']).to eq(@dashboard['user_id'])
+        expect(json_response['data']['attributes']['private']).to eq(@dashboard['private'])
+        expect(json_response['data']['attributes']['production']).to eq(@dashboard['production'])
+        expect(json_response['data']['attributes']['preproduction']).to eq(@dashboard['preproduction'])
+        expect(json_response['data']['attributes']['staging']).to eq(@dashboard['staging'])
+        expect(json_response['data']['attributes']['application']).to eq(@dashboard['application'])
+        expect(json_response['data']['attributes']['is-highlighted']).to eq(@dashboard['is_highlighted'])
+        expect(json_response['data']['attributes']['is-featured']).to eq(@dashboard['is_featured'])
       end
     end
 
-    it 'clones the dashboard overriding data provided in the request body' do
+    it 'returns 200 OK with the created dashboard data when providing valid data in request body (happy case)' do
       VCR.use_cassette('dataset_widget') do
+        new_data = {
+          name: 'Cloned dashboard',
+          summary: 'Cloned summary',
+          description: 'Cloned description',
+          published: false,
+          user_id: USERS[:ADMIN][:id],
+          private: false,
+          production: false,
+          preproduction: false,
+          staging: false,
+        }
+
         post 'clone', params: {
           id: @dashboard.id,
           loggedUser: USERS[:ADMIN],
-          data: {
-            "attributes": {
-              "user-id": USERS[:ADMIN][:id]
-            }
-          },
+          data: { attributes: new_data },
         }
+
         expect(response.status).to eq(200)
-        expect(JSON.parse(response.body)['data']['attributes']['user-id']).to eq(USERS[:ADMIN][:id])
+        json_response = JSON.parse(response.body)
+
+        expect(json_response['data']['id']).to satisfy { |new_id| new_id != @dashboard['id'] }
+        expect(json_response['data']['attributes']['slug']).to satisfy { |new_slug| new_slug != @dashboard['slug'] }
+
+        expect(json_response['data']['attributes']['application']).to eq(@dashboard['application'])
+        expect(json_response['data']['attributes']['is-highlighted']).to eq(@dashboard['is_highlighted'])
+        expect(json_response['data']['attributes']['is-featured']).to eq(@dashboard['is_featured'])
+
+        expect(json_response['data']['attributes']['name']).to eq(new_data[:name])
+        expect(json_response['data']['attributes']['summary']).to eq(new_data[:summary])
+        expect(json_response['data']['attributes']['description']).to eq(new_data[:description])
+        expect(json_response['data']['attributes']['published']).to eq(new_data[:published])
+        expect(json_response['data']['attributes']['user-id']).to eq(new_data[:user_id])
+        expect(json_response['data']['attributes']['private']).to eq(new_data[:private])
+        expect(json_response['data']['attributes']['production']).to eq(new_data[:production])
+        expect(json_response['data']['attributes']['preproduction']).to eq(new_data[:preproduction])
+        expect(json_response['data']['attributes']['staging']).to eq(new_data[:staging])
+      end
+    end
+
+    it 'returns 200 OK with valid cloned dashboard (with original values kept) when providing invalid override data in request body' do
+      VCR.use_cassette('dataset_widget') do
+        new_data = {
+          id: '999',
+          slug: 'slug',
+          is_highlighted: true,
+          is_featured: true,
+          application: ['fake-app'],
+        }
+
+        post 'clone', params: {
+          id: @dashboard.id,
+          loggedUser: USERS[:ADMIN],
+          data: { attributes: new_data },
+        }
+
+        expect(response.status).to eq(200)
+        json_response = JSON.parse(response.body)
+        expect(json_response['data']['id']).to satisfy { |new_id| new_id != @dashboard['id'] and new_id != new_data[:id] }
+        expect(json_response['data']['attributes']['slug']).to satisfy { |new_slug| new_slug != @dashboard['slug'] and new_slug != new_data[:slug] }
+        expect(json_response['data']['attributes']['application']).to eq(@dashboard['application'])
+        expect(json_response['data']['attributes']['is-highlighted']).to eq(@dashboard['is_highlighted'])
+        expect(json_response['data']['attributes']['is-featured']).to eq(@dashboard['is_featured'])
       end
     end
   end
