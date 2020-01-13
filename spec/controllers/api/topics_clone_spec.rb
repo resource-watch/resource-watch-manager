34ce7dc07de09e6b59e2c5e8b349bea42bb775fd
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-01-13 10:47:32 +0000

    Add tests for cloning dashboards from topics feature

--- spec/controllers/api/topics_clone_spec.rb
+++ spec/controllers/api/topics_clone_spec.rb
@@ -7,13 +7,34 @@ require 'constants'
 describe Api::TopicsController, type: :controller do
   describe 'POST #topic clone endpoint' do
     before(:each) do
-      FactoryBot.create :topic_with_widgets
+      @topic = FactoryBot.create :topic_with_widgets
     end
 
     it 'should perform the cloning' do
       VCR.use_cassette('dataset_widget') do
-        post 'clone', params: {id: '1', loggedUser: USERS[:ADMIN]}
+        post 'clone', params: {id: @topic.id, loggedUser: USERS[:ADMIN]}
         expect(response.status).to eq(200)
+      end
+    end
+
+    it 'returns 404 Not Found when creating a dashboard clone from a topic that does not exist' do
+      VCR.use_cassette('dataset_widget') do
+        post 'clone_dashboard', params: {id: '999', loggedUser: USERS[:ADMIN]}
+        expect(response.status).to eq(404)
+      end
+    end
+
+    it 'returns 200 OK with the created dashboard when creating a dashboard clone from a topic' do
+      VCR.use_cassette('dataset_widget') do
+        post 'clone_dashboard', params: {id: @topic.id, loggedUser: USERS[:ADMIN]}
+        expect(response.status).to eq(200)
+        clonedDashboard = json_response[:data]
+        validate_dashboard_structure(clonedDashboard)
+        expect(clonedDashboard[:attributes][:name]).to eq(@topic.name)
+        expect(clonedDashboard[:attributes][:description]).to eq(@topic.description)
+        expect(clonedDashboard[:attributes][:published]).to eq(@topic.published)
+        expect(clonedDashboard[:attributes][:application]).to eq(@topic.application)
+        expect(clonedDashboard[:attributes]["user-id".to_sym]).to eq(USERS[:ADMIN][:id])
       end
     end
   end
