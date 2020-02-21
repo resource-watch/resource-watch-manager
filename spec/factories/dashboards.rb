d8c946d0b865dedc3ead444fe7adbbab61edb259
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-02-21 16:13:24 +0000

    Add failing test for cloning dashboards without widgets

--- spec/factories/dashboards.rb
+++ spec/factories/dashboards.rb
@@ -96,4 +96,14 @@ FactoryBot.define do
     is_featured { false }
     content { "[{\"id\":1520242542490,\"type\":\"text\",\"content\":\"\\u003cp\\u003eBiodiversity intro\\u003c/p\\u003e\"},{\"id\":1518109294215,\"type\":\"widget\",\"content\":{\"widgetId\":\"841a6544-e8c9-411d-9987-83e81b58fd6f\",\"datasetId\":\"9aa17362-2a4f-4a4f-9e4e-97ebb60ce76b\",\"categories\":[]}},{\"id\":1518109371974,\"type\":\"widget\",\"content\":{\"widgetId\":\"841a6544-e8c9-411d-9987-83e81b58fd6f\",\"datasetId\":\"9aa17362-2a4f-4a4f-9e4e-97ebb60ce76b\",\"categories\":[]}},{\"id\":1518109389591,\"type\":\"widget\",\"content\":{\"widgetId\":\"841a6544-e8c9-411d-9987-83e81b58fd6f\",\"datasetId\":\"9aa17362-2a4f-4a4f-9e4e-97ebb60ce76b\",\"categories\":[]}},{\"id\":1518109424849,\"type\":\"widget\",\"content\":{\"widgetId\":\"841a6544-e8c9-411d-9987-83e81b58fd6f\",\"datasetId\":\"9aa17362-2a4f-4a4f-9e4e-97ebb60ce76b\",\"categories\":[]}}]" }
   end
+
+  factory :dashboard_without_widgets, class: Dashboard do
+    name { FFaker::Name.name }
+    published { true }
+    user_id { '5c143429f8d19932db9d06ea' }
+    private { false }
+    is_highlighted { false }
+    is_featured { false }
+    content { nil }
+  end
 end
