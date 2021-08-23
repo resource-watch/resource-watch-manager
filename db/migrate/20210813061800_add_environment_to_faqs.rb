62eba11b1c04c69400990997adcac894b93853d4
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-08-23 16:54:50 +0200

    Added forgotten faq specs

--- db/migrate/20210813061800_add_environment_to_faqs.rb
+++ db/migrate/20210813061800_add_environment_to_faqs.rb
@@ -1,8 +1,12 @@
 class AddEnvironmentToFaqs < ActiveRecord::Migration[5.1]
   def change
     add_column :faqs, :environment, :text, default: Environment::PRODUCTION
-    # apply default to existing records
-    execute "UPDATE faqs SET environment='#{Environment::PRODUCTION}'"
+    reversible do |dir|
+      dir.up do
+        # apply default to existing records
+        execute "UPDATE faqs SET environment='#{Environment::PRODUCTION}'"
+      end
+    end
     # set NOT NULL
     change_column_null :faqs, :environment, false
   end
