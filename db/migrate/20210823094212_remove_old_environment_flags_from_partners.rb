d221347128440ec51a45d2acb24f3e000718c807
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2021-08-24 13:36:34 +0200

    Fixed reversing migration, to allow re-populating the old flags if need be

--- db/migrate/20210823094212_remove_old_environment_flags_from_partners.rb
+++ db/migrate/20210823094212_remove_old_environment_flags_from_partners.rb
@@ -1,5 +1,17 @@
 class RemoveOldEnvironmentFlagsFromPartners < ActiveRecord::Migration[5.1]
   def change
+    reversible do |dir|
+      dir.down do
+        # set env flags based on environment value
+        execute <<~SQL
+          UPDATE partners
+          SET production = CASE WHEN environment = '#{Environment::PRODUCTION}' THEN TRUE ELSE FALSE END,
+              preproduction = CASE WHEN environment = 'preproduction' THEN TRUE ELSE FALSE END,
+              staging = CASE WHEN environment = 'staging' THEN TRUE ELSE FALSE END
+        SQL
+      end
+    end
+
     # environment has been populated based on the values of these columns in previous migration
     remove_column :partners, :production, :boolean, default: true
     remove_column :partners, :preproduction, :boolean, default: false
--- db/migrate/20210823123210_remove_old_environment_flags_from_tools.rb
+++ db/migrate/20210823123210_remove_old_environment_flags_from_tools.rb
@@ -1,5 +1,17 @@
 class RemoveOldEnvironmentFlagsFromTools < ActiveRecord::Migration[5.1]
   def change
+    reversible do |dir|
+      dir.down do
+        # set env flags based on environment value
+        execute <<~SQL
+          UPDATE tools
+          SET production = CASE WHEN environment = '#{Environment::PRODUCTION}' THEN TRUE ELSE FALSE END,
+              preproduction = CASE WHEN environment = 'preproduction' THEN TRUE ELSE FALSE END,
+              staging = CASE WHEN environment = 'staging' THEN TRUE ELSE FALSE END
+        SQL
+      end
+    end
+
     # environment has been populated based on the values of these columns in previous migration
     remove_column :tools, :production, :boolean, default: true
     remove_column :tools, :preproduction, :boolean, default: false
