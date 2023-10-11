d154a552ae4cb7dac1cb9da857c1010e2e70ffd6
Tiago Garcia <tiagojsag@gmail.com>
2023-10-11 09:43:21 +0200

    Split S3 and CloudWatch access credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,8 +100,8 @@ Rails.application.configure do
     s3_region: ENV.fetch('AWS_REGION'),
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
-      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
+      access_key_id: ENV.fetch('S3_AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('S3_AWS_SECRET_ACCESS_KEY')
     },
     s3_permissions: 'public-read'
   }
--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,9 @@
 aws_config = {
-  region: ENV.fetch('AWS_REGION'),
+  region: ENV.fetch('CLOUDWATCH_AWS_REGION', 'us-east-1'),
 }
 
-if ENV['AWS_ACCESS_KEY_ID'].present? && ENV['AWS_SECRET_ACCESS_KEY'].present?
-  aws_config[:credentials] = Aws::Credentials.new(ENV.fetch('AWS_ACCESS_KEY_ID'), ENV.fetch('AWS_SECRET_ACCESS_KEY'))
+if ENV['CLOUDWATCH_AWS_ACCESS_KEY_ID'].present? && ENV['CLOUDWATCH_AWS_SECRET_ACCESS_KEY'].present?
+  aws_config[:credentials] = Aws::Credentials.new(ENV.fetch('CLOUDWATCH_AWS_ACCESS_KEY_ID'), ENV.fetch('CLOUDWATCH_AWS_SECRET_ACCESS_KEY'))
 end
 
 Aws.config.update(aws_config)
